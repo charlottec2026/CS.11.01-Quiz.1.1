2b10d8e00472f089973eaa88e51bbf6a02813e5c
charlottec2026 <charlottec2026@student.cis.edu.hk>
2024-02-05 08:16:45 +0800

    written code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter your first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Please enter your favourite food: ");
+        String favouriteFood = scanner.nextLine();
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
+    }
     }
 
-}
